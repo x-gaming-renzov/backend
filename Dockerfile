326957e87b798e1087bbd4b60026a26713252a7e
pixelatedBrad <paras@sif.design>
2024-11-27 00:04:13 +0530

    Update Dockerfile entrypoint to CMD and refactor Google credentials loading to use environment variables

--- Dockerfile
+++ Dockerfile
@@ -35,4 +35,4 @@ EXPOSE 8501
 # Command to run the application
 HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
 
-ENTRYPOINT ["streamlit", "run", "streamlit_app.py", "--server.port=8501", "--server.address=0.0.0.0"]
+CMD ["streamlit", "run", "streamlit_app.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- main.py
+++ main.py
@@ -9,7 +9,18 @@ from src.flat.main import flatten_json_leaving_lists
 
 dotenv.load_dotenv()
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name("gcreds.json")
+credentials = ServiceAccountCredentials.from_json_keyfile_dict({
+    "type": os.getenv("TYPE"),
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY"),
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
+    "auth_uri": os.getenv("AUTH_URI"),
+    "token_uri": os.getenv("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL")
+})
 client = storage.Client(credentials=credentials, project="assetgeneration")
 bucket = client.get_bucket("xg_live_ops")
 
