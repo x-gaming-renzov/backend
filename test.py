f2848f5d598a275568227444942de9cecdf623ee
pixelatedBrad <paras@sif.design>
2024-11-27 20:15:23 +0530

    Refactor file upload functionality to use Google Cloud Storage; add asynchronous cleanup and upload methods with callback support

--- test.py
+++ test.py
@@ -1,17 +1,59 @@
+from gcloud import storage
+from oauth2client.service_account import ServiceAccountCredentials
+from termcolor import colored
+import os
 
-    upload_input_files("original.json", "application/json", USER_ID, USER_SESSION_ID)
-    upload_input_files(graph_input["file_name"], "application/json", USER_ID, USER_SESSION_ID)
-    print("Files uploaded")
+credentials = ServiceAccountCredentials.from_json_keyfile_name("gcreds.json")
+client = storage.Client(credentials=credentials, project="assetgeneration")
+bucket = client.get_bucket("xg_live_ops")
 
-    data_blob = bucket.blob(f"""{graph_input["user_id"]}/{graph_input["user_session_id"]}/{graph_input["file_name"]}""")
-    data_blob.download_to_filename(f"""temp/{graph_input["user_id"]}/{graph_input["user_session_id"]}/{graph_input["file_name"]}""")
+def upload_input_files(file_name, file_type, USER_ID, USER_SESSION_ID):
+    user_id = USER_ID
+    user_session_id = USER_SESSION_ID
+    file_name = file_name
 
-    kb_urls_blob = bucket.blob(f"""{graph_input["user_id"]}/{graph_input["user_session_id"]}/kb_urls.json""")
-    if kb_urls_blob.exists():
-        pathlib.Path(temp_dir_path + "/kb_urls.json").touch()
-        kb_urls_blob.download_to_filename(f"""temp/{graph_input["user_id"]}/{graph_input["user_session_id"]}/kb_urls.json""")
+    print(colored(f"Uploading file: {file_name}", "green"))
 
-    kb_data_blob = bucket.blob(f"""{graph_input["user_id"]}/{graph_input["user_session_id"]}/kb_data.txt""")
-    if kb_data_blob.exists():
-        pathlib.Path(temp_dir_path + "/kb_data.txt").touch()
-        kb_data_blob.download_to_filename(f"""temp/{graph_input["user_id"]}/{graph_input["user_session_id"]}/kb_data.txt""")
+    blob = bucket.blob(f"""{user_id}/{user_session_id}/{file_name}""")
+    blob.upload_from_filename(os.getcwd() + f"""/temp/{user_id}/{user_session_id}/{file_name}""", content_type=file_type)
+    blob.metadata = { "xg_live_ops" : "attachment", "content-disposition" : "attachment" }
+    blob.patch()
+    blob.content_disposition = f"attachment; filename={file_name}"
+    blob.patch()
+
+    print(colored(f"File uploaded to: {blob.public_url}", "green"))
+
+# Modify upload_all_files to accept callback
+def upload_all_files_async(USER_ID, USER_SESSION_ID, callback=None):
+    user_id = USER_ID
+    user_session_id = USER_SESSION_ID
+
+    print(colored(f"Uploading all files for user_id: {user_id} and user_session_id: {user_session_id}", "green"))
+
+    for root, dirs, files in os.walk(os.getcwd() + f"""/temp/{user_id}/{user_session_id}"""):
+        for file in files:
+            if file != "out.json":
+                upload_input_files(file, "application/json", user_id, user_session_id)
+
+    print(colored("Files uploaded", "green"))
+    if callback:
+        callback("Upload completed")
+
+# Modify cleanup_files_dir to accept callback
+def cleanup_files_dir_async(USER_ID, USER_SESSION_ID, callback=None):
+    user_id = USER_ID
+    user_session_id = USER_SESSION_ID
+
+    print(colored(f"Cleaning up files for user_id: {user_id} and user_session_id: {user_session_id}", "green"))
+
+    for root, dirs, files in os.walk(os.getcwd() + f"""/temp/{user_id}/{user_session_id}"""):
+        for file in files:
+            os.remove(os.path.join(root, file))
+                
+    for root, dirs, files in os.walk(os.getcwd() + f"""/temp/{user_id}/{user_session_id}"""):
+        for dir in dirs:
+            os.rmdir(os.path.join(root, dir))
+
+    print(colored("Files cleaned up", "green"))
+    if callback:
+        callback("Cleanup completed")
